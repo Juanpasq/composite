181b047d2913ea326be4d15d1ba03a8fedec8d3b
Juan Manuel Pasquali <juan.pasquali@fluxit.com.ar>
2022-09-21 02:07:50 -0300

    first

---
+++ src/Empleado.java
@@ -0,0 +1,10 @@
+
+public interface Empleado {
+	
+	public void add(Empleado empleado);
+	public String getNombre();
+	public double getSalario();
+	public void print();
+	public void delete(Empleado empleado);
+
+}
